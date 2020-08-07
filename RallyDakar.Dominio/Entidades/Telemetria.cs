ca07ce2e12c828e88d8c6a97f1f663c1d6a726bd
Nilson Ricardo da Silva <silva.nilsonr@gmail.com>
2020-08-07 17:16:12 -0300

    Primeira versão

---
+++ RallyDakar.Dominio/Entidades/Telemetria.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RallyDakar.Dominio.Entidades
+{
+    public class Telemetria
+    {
+        public int Id { get; set; }
+        public int EquipeId { get; set; }
+        public DateTime Data { get; set; }
+        public DateTime DataServidor { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public decimal PercentualCombustivel { get; set; }
+        public double Velocidade { get; set; }
+        public double RPM { get; set; }
+        public int TemperaturaExterna { get; set; }
+        public int TemperaturaMotor { get; set; }
+        public double AltitudeNivelMar { get; set; }
+        public bool PedalAcelerador { get; set; }
+        public bool PedalFreio { get; set; }
+    }
+}
